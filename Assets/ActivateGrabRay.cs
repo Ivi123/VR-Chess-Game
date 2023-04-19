321d70bd1f3ecdb05f4c57ce81acd93f4923953d
Ivona <ivona_racasean9@yahoo.com>
2023-04-19 21:39:22 +0300

    First commit

---
+++ Assets/ActivateGrabRay.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;  
+
+public class ActivateGrabRay : MonoBehaviour
+{
+    public GameObject leftGrabRayObject;
+    public GameObject rightGrabRayObject;
+
+
+    public XRDirectInteractor leftDirectInteractor;
+    public XRDirectInteractor rightDirectInteractor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftGrabRayObject.SetActive(leftDirectInteractor.interactablesSelected.Count == 0);
+        rightGrabRayObject.SetActive(rightDirectInteractor.interactablesSelected.Count == 0);
+
+    }
+}
